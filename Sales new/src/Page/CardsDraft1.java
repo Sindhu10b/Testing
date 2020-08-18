c2dae21eb532f48602f0fa7b391087b577ea6a8f
sindhu <sindhu@HPS192-PC.hpsad.com>
2020-08-18 19:40:48 +0530

    Project name

--- Sales new/src/Page/CardsDraft1.java
+++ Sales new/src/Page/CardsDraft1.java
@@ -1,14 +1,11 @@
 package Page;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CardsDraft1 {
 	 WebDriver driver;
